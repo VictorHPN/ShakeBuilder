ae9556d7146f742150aa3a5d017734a90df73c16
Victor Hugo Polli Neves <victor.polli.neves@gmail.com>
2022-10-23 22:58:11 -0300

    Adding RFID payment system V1.

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,11 @@ static Estados estado = ESTADO_INICIO;
 
 /*=================== Variáveis de controle ===================*/
 bool continue_pressed = false; // controle do botão de continue
-
 Shake shake; // Objeto do shake a ser preparado
 
 /*======================= Configs LCD =========================*/
 LiquidCrystal_I2C lcd(0x27, 20, 4); // Inicialização Display LCD
-// Funções:
+// Funções LCD:
 void Tela_incio();
 void Tela_suplemento();
 void Tela_suplemento_atualizada(float preco, int id_sup, int doses, bool leite);
@@ -34,19 +33,16 @@ void Tela_preparo_servindo();
 void Tela_fim();
 
 /*======================= Configs RFID =========================*/
+#define ID "8C 07 02 04"
+#define LED_RFID 13
+#define SS_PIN 5
+#define RST_PIN 15
+//Objeto módulo RFID:
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+// Funções RFID
+bool PagamentoRFID();
 
-// #define CS_PIN 5
-// #define RST_PIN 0
-
-// //esse objeto 'chave' é utilizado para autenticação
-// MFRC522::MIFARE_Key key;
-// //código de status de retorno da autenticação
-// MFRC522::StatusCode status;
-// // Definicoes pino modulo RC522
-// MFRC522 rfid(CS_PIN, RST_PIN);
-
-// int Pagamento(MFRC522 rfid);
-
+/*=============================================================*/
 
 void setup()
 {
@@ -68,13 +64,15 @@ void setup()
   lcd.begin();
   lcd.backlight();
 
-  /*================ Inicialização RFID ================*/
-  // Serial.begin(9600);
-  // SPI.begin(); // Init SPI bus
-  // // Inicia MFRC522S
-  // rfid.PCD_Init();
+  /*=================== Inicialização RFID ====================*/
+  // inicia a comunicacao SPI que sera usada para comunicacao com o mudulo RFID
+  SPI.begin();
+  mfrc522.PCD_Init();  //inicia o modulo RFID
 
-//--------------------------------------------------------//
+  //Monitoramento RFID:
+  Serial.begin(115200); 
+  Serial.println("RFID + ESP32");
+  Serial.println("Passe alguma tag RFID para verificar o id da mesma.");
 }
 
 void loop()
@@ -102,7 +100,7 @@ void loop()
           Tela_suplemento_atualizada(shake.get_preco_total(), shake.get_id_suplemento(),
                                     shake.get_doses(), shake.get_leite_st());
           delay(400);
-          while(digitalRead(button_state[i]) == HIGH){}
+          while(digitalRead(button_state[i]) == HIGH){} // Aguarda botão ser solto para continuar leitura
         }
       }
       if (shake.get_id_suplemento() != -1)
@@ -117,10 +115,13 @@ void loop()
     break;
 
   case ESTADO_PAGAMENTO:
-    if (1) // Avança para o prepario somente se identificar o pagamaento
+    while(PagamentoRFID() == false) // Avança para o prepario somente se identificar o pagamaento
     {
+        Tela_pagamento_aguardando();
     }
     lcd.clear(); // Limpa display
+    Tela_pagamento_confirmado();
+    delay(3000);
     estado = ESTADO_PREPARO;  // Avança para próximo estado
     break;
 
@@ -258,4 +259,41 @@ void Tela_fim()
   lcd.print("Compra! :)");
   lcd.setCursor(0, 3);
   lcd.print("#==================#");
+}
+
+bool PagamentoRFID()
+{
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+     return false;                 // se nao tiver um cartao para ser lido recomeça a função
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return false;                  //se nao conseguir ler o cartao recomeça a função tambem
+  }
+
+  String conteudo = "";      // cria uma string
+
+  Serial.print("id da tag :"); //imprime na serial o id do cartao
+
+  for (byte i = 0; i < mfrc522.uid.size; i++){       // faz uma verificacao dos bits da memoria do cartao
+    //ambos comandos abaixo vão concatenar as informacoes do cartao...
+    //porem os 2 primeiros irao mostrar na serial e os 2 ultimos guardarao os valores na string de conteudo para fazer as verificacoes
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  conteudo.toUpperCase();                      // deixa as letras da string todas maiusculas
+
+  if (conteudo.substring(1) == ID)// verifica se o ID do cartao lido tem o mesmo ID do cartao que queremos liberar o acesso
+  {
+    Serial.print("Cartão reconhecido -> Pagamento CONFIRMADO!");
+    digitalWrite(LED_RFID, HIGH); // Indicativo que o pagamento foi confirmado
+    return true;
+  }
+  else // caso o cartao lido não foi registrado
+  {
+    Serial.print("Cartão não registrado -> ID não cadastrada.");
+    return false;
+  }
 }
